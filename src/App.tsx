29d6be3cc0c9d8f3ee03a04280a11fb313e80be7
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-04-20 23:53:21 +0530

    functional components in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Component} from "react";
 
 
-class App extends Component {
-  render(){
+
+function App(){
     return(
       <>
       <h1>hello word!!</h1>
       </>
     )
-  }
+  
 }
 
 export default App;
